6725935323e4da4d3402dfc8a8aa123c9ae82860
Roman Mukhin <roman11mukhin@gmail.com>
2013-03-12 12:37:20 +0200

    Update main_controller.rb

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,17 +6,14 @@ class MainController < ApplicationController
     User.authorization(@user.screen_name, @user.uid, session[:token])
     session[:current_user] = @user.uid.to_s
     
-    if @user_vkontakte = User.find_by_uid(@user.uid.to_s)
-      @lists = @user_vkontakte.lists
-      search_my_music
-      unless @user_vkontakte.lists.all.first.nil? 
-        @list = ListDecorator.find(session[:current_list] ||= @user_vkontakte.lists.all.first.id)
-      end
-      @audios = Kaminari.paginate_array(@audios).page(params[:page]).per(20)
-      @user = UserDecorator.decorate(@user)
-    else
-      render_404
+    @user_vkontakte = User.find_by_uid(@user.uid.to_s)
+    @lists = @user_vkontakte.lists
+    search_my_music
+    unless @user_vkontakte.lists.all.first.nil? 
+      @list = ListDecorator.find(session[:current_list] ||= @user_vkontakte.lists.all.first.id)
     end
+    @audios = Kaminari.paginate_array(@audios).page(params[:page]).per(20)
+    @user = UserDecorator.decorate(@user)
   end 
 
   def search
@@ -33,14 +30,5 @@ class MainController < ApplicationController
   def download
     resp = download_audio(params[:url])
     temp_file_sending(song_name(params[:name],params[:url]), resp)
-  end
-
-  protected
-  def render_404
-    respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found, :layout => false}
-      format.xml  { head :not_found }
-      format.any  { head :not_found }
-    end
   end
 end
