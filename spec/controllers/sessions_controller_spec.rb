2b9a42e91c587d77f88cba1a3d4648420b35a67c
Roman Mukhin <roman11mukhin@gmail.com>
2013-03-18 09:52:32 +0200

    Update sessions_controller_spec.rb

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,28 +4,28 @@ describe SessionsController do
   let(:list){ FactoryGirl.create(:list) }
   let(:songs){ FactoryGirl.create_list(:song,3, list_id: list.id)} 
   before(:each) do
-	session[:current_list] = list.id 
+    session[:current_list] = list.id 
   end
 
   context "#new" do
-  	it "opens new session" do
-  	  controller.should_receive(:srand)
-  	  post :new
-  	  session[:state].should_not be_blank
-  	end
+    it "opens new session" do
+      controller.should_receive(:srand)
+      post :new
+      session[:state].should_not be_blank
+    end
 
-  	it "begins omniauth process" do
+    it "begins omniauth process" do
       VkontakteApi.stub(:authorization_url).and_return("http://oauth.vk.com/state?somthing&234")
       VkontakteApi.should_receive(:authorization_url).with(scope: [:offline, :audio], state: anything())
       post :new
       assigns[:vk_url].should == "http://oauth.vk.com/state?somthing&234"
-  	end
+    end
   end
 
   context "#callback" do
-  	before(:each) do
-  	  session[:state] = '11111111'
-  	end
+    before(:each) do
+      session[:state] = '11111111'
+    end
 
     it "redirects to root url if session not equils params" do 
       get :callback, state: '22222222'
@@ -52,16 +52,16 @@ describe SessionsController do
       session[:vk_id] = "something"
     end
 
-    it"destroys session" do
+    it "destroys session" do
       delete :destroy
       User.find(@user.id).token.should be_nil
       session[:token].should be_nil
       session[:vk_id].should be_nil
-  	end
+    end
 
-  	it"redirects to root" do 
+    it "redirects to root" do 
       delete :destroy
       response.should redirect_to root_url
-  	end
+    end
   end
 end
